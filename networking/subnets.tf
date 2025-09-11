64643fc778fc87b1c398fe942af6e37f2fc54b7d
cooj-gft <cooj@gft.com>
2025-09-11 15:36:14 -0500

    Corregir AZ

--- networking/subnets.tf
+++ networking/subnets.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "private_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = false
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-2b"
   map_public_ip_on_launch = false
 
   tags = {
